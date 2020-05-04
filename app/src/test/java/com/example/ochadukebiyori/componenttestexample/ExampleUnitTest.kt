842d8ca4256af0bf4ddbead4627b046cd22d1b1b
Pickles Ochaduke <pickles.ochazuke@gmail.com>
2020-05-04 17:46:12 +0900

    RobolectricでActivityのUIテストを行えるように環境構築

--- app/src/test/java/com/example/ochadukebiyori/componenttestexample/ExampleUnitTest.kt
+++ app/src/test/java/com/example/ochadukebiyori/componenttestexample/ExampleUnitTest.kt
@@ -1,17 +1,33 @@
 package com.example.ochadukebiyori.componenttestexample
 
-import org.junit.Test
+// Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 
+// JUnit
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
+
+    @get:Rule
+    val mainScenarioRule = activityScenarioRule<MainActivity>()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun 起動したらHelloWorldが表示されるべき() {
+        onView(withId(R.id.helloWorld)).check(matches(withText("Hello World!")))
     }
 }
